f1e5ba7bfde9e96603ff5e321fe7c96fd7b13d8d
Abhishek Laddha <abhishek@Abhisheks-MacBook-Air-2.local>
2025-02-11 09:21:26 +0530

    Initial commit

---
+++ backend/admin/assets/inc/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                2020 - <?php echo date('Y'); ?> &copy; Hospital Management System. Developed By Students of NIT Jalandhar</a>
+            </div>
+
+        </div>
+    </div>
+</footer>
---
+++ backend/doc/assets/inc/footer1.php
@@ -0,0 +1,3 @@
+<footer class="footer footer-alt">
+            2020 - <?php echo date ('Y');?> &copy; Hospital Management System. Developed By Students of NIT Jalandhar</a> 
+</footer>
